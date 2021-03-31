5f6946ccc149eadb215bb2720f8976a95145e4f7
Andra Beje <63416838+Andra10@users.noreply.github.com>
2021-03-31 23:50:37 +0200

    Update script.js

--- Menu/script.js
+++ Menu/script.js
@@ -204,4 +204,96 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
-})
+});
+
+const header = document.getElementsByClassName('.brand-title');
+
+
+let textWrapper = document.querySelector('.ml6 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml6 .letter',
+    translateY: ["1.1em", 0],
+    translateZ: 0,
+    duration: 750,
+    delay: (el, i) => 50 * i
+  }).add({
+    targets: '.ml6',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+
+  const sectionCenter = document.querySelector(".section-center");
+  const btnContainer = document.querySelector(".btn-container");
+  // display all items when page loads
+  window.addEventListener("DOMContentLoaded", function () {
+    diplayMenuItems(menu);
+    displayMenuButtons();
+  });
+  
+  function diplayMenuItems(menuItems) {
+    let displayMenu = menuItems.map(function (item) {
+      // console.log(item);
+  
+      return `<article class="menu-item">
+            <img src=${item.img} alt=${item.title} class="photo" />
+            <div class="item-info">
+              <header>
+                <h4>${item.title}</h4>
+                <h4 class="price">$${item.price}</h4>
+              </header>
+              <p class="item-text">
+                ${item.desc}
+              </p>
+            </div>
+          </article>`;
+    });
+    displayMenu = displayMenu.join("");
+    // console.log(displayMenu);
+  
+    sectionCenter.innerHTML = displayMenu;
+  }
+  function displayMenuButtons() {
+    const categories = menu.reduce(
+      function (values, item) {
+        if (!values.includes(item.category)) {
+          values.push(item.category);
+        }
+        return values;
+      },
+      ["all"]
+    );
+    const categoryBtns = categories
+      .map(function (category) {
+        return `<button type="button" class="filter-btn" data-id=${category}>
+            ${category}
+          </button>`;
+      })
+      .join("");
+  
+    btnContainer.innerHTML = categoryBtns;
+    const filterBtns = btnContainer.querySelectorAll(".filter-btn");
+    console.log(filterBtns);
+  
+    filterBtns.forEach(function (btn) {
+      btn.addEventListener("click", function (e) {
+        // console.log(e.currentTarget.dataset);
+        const category = e.currentTarget.dataset.id;
+        const menuCategory = menu.filter(function (menuItem) {
+          // console.log(menuItem.category);
+          if (menuItem.category === category) {
+            return menuItem;
+          }
+        });
+        if (category === "all") {
+          diplayMenuItems(menu);
+        } else {
+          diplayMenuItems(menuCategory);
+        }
+      });
+    });
+  }
